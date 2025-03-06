b01d166809635be63cf5dcb5292e611256b56f6b
yoon <sukanyat2022a@gmail.com>
2025-03-06 12:55:04 -0500

    add function show green border

--- src/componentFolder/FilterTypeCarComp.tsx
+++ src/componentFolder/FilterTypeCarComp.tsx
@@ -1,17 +1,25 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useState} from 'react';
 import {myColor} from '../constant/color';
+import Icon from 'react-native-vector-icons/FontAwesome';
 interface FilterItemProp {
   label: string;
   isSelected: boolean;
   onPress: () => void;
 }
 
-//below create only one box
+//below create only one box and we use a box to map
 const FilterItemComp = ({label, isSelected, onPress}: FilterItemProp) => {
   return (
-    <TouchableOpacity style={styles.mainBox} onPress={onPress}>
-      <Text>{label}</Text>
+    <TouchableOpacity style={[styles.mainBox, isSelected ? styles.afterSelected: '']}
+    //isSelected ? styles.afterSelected: '' and isSelected && styles.afterSelected is the same
+    //isSelected && styles.afterSelected -- if user seleted is true
+    // // and it excecute it change color style to green
+    //if front condition isselected is true and then after && excute style
+    onPress={onPress}>
+      <Text style={isSelected ? styles.chageColorLabelAfterClick: ''}>{label}</Text>
+      {/* //only box is seleted it show icon x */}
+      {isSelected && <Icon name="close" size={15} color={myColor.greenColor} />}
     </TouchableOpacity>
   );
 };
@@ -21,6 +29,7 @@ const FilterTypeCarComp = () => {
   const selectedFunc = (item: string) => {
     setSelectedValue(item);
   };
+  console.log(selectedValue)
   return (
     //map te box
     <View style={styles.wrapper}>
@@ -30,6 +39,7 @@ const FilterTypeCarComp = () => {
             key={index}
             label={item}
             isSelected={selectedValue === item}
+            //boolean if user click/selected is boolean it create bordercolor to green--above
             onPress={() => selectedFunc(item)}
           />
         );
@@ -66,7 +76,16 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
     borderRadius: 10,
+    flexDirection:'row',
+    gap:2,
   },
+  afterSelected:{
+    borderColor:myColor.greenColor,
+  },
+  chageColorLabelAfterClick:{
+    color:myColor.greenColor,
+  }
+
 });
 // greenColor: "#0DB184",
 // lightgreenClolor:"#EBF9F5"
